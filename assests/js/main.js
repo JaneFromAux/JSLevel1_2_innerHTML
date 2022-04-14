a7676bb372c61db3b6af32ab79e3fe2687a67308
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-14 13:26:48 +0200

    durch

---
+++ assests/js/main.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('info').innerHTML = '<h1>Hello World</h1>';
+
+document.getElementById('howAreYou').innerHTML = "<h2>How are you?</h2>";
+
+document.getElementById('container').innerHTML = "<p>Start of the Element</p>"
+
+document.write('End of the Element');
